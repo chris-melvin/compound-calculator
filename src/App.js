305e5881ffbf13e5759fb03016e0da8fe99e0b80
Melvin Factoriza <chrismelvin.04@gmail.com>
2020-11-30 18:21:04 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import { First, Page1 } from './pages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <h1>Building wealth in your twenties</h1>
+        <Switch>
+          <Route path="/" exact component={First} />
+        </Switch>
+      </Router>
     </div>
   );
 }
